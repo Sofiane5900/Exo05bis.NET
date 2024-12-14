00351b2dd2a3e882434729850728a078afc5d131
Sofiane <iguedjetalsofiane@gmail.com>
2024-12-14 12:55:19 +0100

    Add project files.

---
+++ ExerciceInsertionDeValeurs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ExerciceInsertionDeValeurs // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tableau = [10];
+
+            Console.WriteLine("Insertion des valeurs du tableau :");
+
+            int compteur = 0;
+            while (compteur < tableau.Length)
+            {
+                Console.Write($"Saisir la valeur {compteur} : ");
+                tableau[compteur] = int.Parse(Console.ReadLine());
+                compteur++;
+
+
+            }
+        }
+    }
+}
